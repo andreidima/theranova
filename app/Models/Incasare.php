979cfcd0d18a4cb3bb31810ff7d3a8c076c2d76c
Andrei Dima <51689479+andreidima@users.noreply.github.com>
2025-10-07 19:20:08 +0200

    Add nr data field to CAS decisions

--- app/Models/Incasare.php
+++ app/Models/Incasare.php
@@ -11,21 +11,61 @@ class Incasare extends Model
 {
     use HasFactory;
 
+    public const TIP_INCASARE = 'incasare';
+    public const TIP_DECIZIE_CAS = 'decizie_cas';
+
     protected $table = 'incasari';
     protected $guarded = [];
 
-    // Specify the data fields that should be treated as Carbon instances
-    protected $dates = ['date'];
+    protected $dates = ['data', 'data_inregistrare', 'data_validare'];
+
     // Mutator to format the data before saving it to the database
     public function setDataAttribute($value)
     {
-        // Assuming the input format is 'd/m/Y' (day/month/year)
+        if (empty($value)) {
+            $this->attributes['data'] = null;
+
+            return;
+        }
+
         $this->attributes['data'] = Carbon::createFromFormat('d.m.Y', $value)->format('Y-m-d');
     }
     // Optionally, you can define an accessor to format the date when retrieving it
     public function getDataAttribute($value)
     {
-        return Carbon::parse($value)->format('d.m.Y');
+        return $value ? Carbon::parse($value)->format('d.m.Y') : null;
+    }
+
+    public function setDataInregistrareAttribute($value)
+    {
+        if (empty($value)) {
+            $this->attributes['data_inregistrare'] = null;
+
+            return;
+        }
+
+        $this->attributes['data_inregistrare'] = Carbon::createFromFormat('d.m.Y', $value)->format('Y-m-d');
+    }
+
+    public function getDataInregistrareAttribute($value)
+    {
+        return $value ? Carbon::parse($value)->format('d.m.Y') : null;
+    }
+
+    public function setDataValidareAttribute($value)
+    {
+        if (empty($value)) {
+            $this->attributes['data_validare'] = null;
+
+            return;
+        }
+
+        $this->attributes['data_validare'] = Carbon::createFromFormat('d.m.Y', $value)->format('Y-m-d');
+    }
+
+    public function getDataValidareAttribute($value)
+    {
+        return $value ? Carbon::parse($value)->format('d.m.Y') : null;
     }
 
     /**
